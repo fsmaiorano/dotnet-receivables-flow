5c6f7382e1915b3f7d6c6762bcc91f6339dca7a6
Fábio Maiorano <fsmaiorano@gmail.com>
2024-05-17 21:04:02 +0100

    create first migration to test relationship

---
+++ src/Infrastructure/Migrations/20240517195841_InitDatabase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assignors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(140)", maxLength: 140, nullable: false),
+                    Document = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "character varying(140)", maxLength: 140, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assignors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payables",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Value = table.Column<float>(type: "real", nullable: false),
+                    EmissionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AssignorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payables", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payables_Assignors_AssignorId",
+                        column: x => x.AssignorId,
+                        principalTable: "Assignors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignors_Email",
+                table: "Assignors",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignors_Id",
+                table: "Assignors",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payables_AssignorId",
+                table: "Payables",
+                column: "AssignorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payables_Id",
+                table: "Payables",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payables");
+
+            migrationBuilder.DropTable(
+                name: "Assignors");
+        }
+    }
+}
