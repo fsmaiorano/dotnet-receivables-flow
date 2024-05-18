ace604ddc4b44dc34cba00861c8e2d15753804fc
Fábio Maiorano <fsmaiorano@gmail.com>
2024-05-18 12:45:40 +0100

    remove unnecessary class

--- src/Application/Common/Models/BaseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Application.Common.Models;
-
-public abstract class BaseDto
-{
-    public Guid Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
-}
