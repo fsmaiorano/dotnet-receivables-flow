3892c1c9576d2b536963652c164b8a3fe38665b6
Fábio Maiorano <fsmaiorano@gmail.com>
2024-05-18 12:34:58 +0100

    create UnitTest project

---
+++ tests/UnitTests/Application/UseCases/Assignor/Commands/CreateAssignorUnitTest.cs
@@ -0,0 +1,51 @@
+using Application.Common.Interfaces;
+using Application.UseCases.Assignor.Commands.CreateAssignor;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace UnitTests.Application.UseCases.Assignor.Commands;
+
+public abstract class CreateAssignorUnitTest
+{
+    [TestFixture]
+    public class CreateAssignorHandlerTests
+    {
+        [Test]
+        public async Task Handle_ValidRequest_CreatesAssignor()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<CreateAssignorHandler>>();
+            var contextMock = new Mock<IDataContext>();
+            var mockSet = new Mock<DbSet<AssignorEntity>>();
+            contextMock.Setup(c => c.Assignors).Returns(mockSet.Object);
+            var handler = new CreateAssignorHandler(loggerMock.Object, contextMock.Object);
+            var command = new CreateAssignorCommand
+                { Name = "Test", Document = "123", Phone = "1234567890", Email = "test@test.com" };
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            contextMock.Verify(c => c.Assignors.Add(It.IsAny<AssignorEntity>()), Times.Once);
+            contextMock.Verify(c => c.SaveChangesAsync(CancellationToken.None), Times.Once);
+            Assert.That(result.Id, Is.Not.EqualTo(Guid.Empty));
+        }
+
+        [Test]
+        public void Handle_ErrorDuringCreation_ThrowsException()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<CreateAssignorHandler>>();
+            var contextMock = new Mock<IDataContext>();
+            contextMock.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>())).Throws<Exception>();
+            var handler = new CreateAssignorHandler(loggerMock.Object, contextMock.Object);
+            var command = new CreateAssignorCommand
+                { Name = "Test", Document = "123", Phone = "1234567890", Email = "test@test.com" };
+
+            // Act & Assert
+            Assert.ThrowsAsync<NullReferenceException>(async () => await handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
