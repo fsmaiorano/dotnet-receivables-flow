01cb4816ff3d79eca18f2d56f4273ce3bd0737ea
Fábio Maiorano <fsmaiorano@gmail.com>
2024-05-23 17:20:47 +0100

    add try catch on MigrationExtensions.cs

--- src/Infrastructure/Extensions/MigrationExtensions.cs
+++ src/Infrastructure/Extensions/MigrationExtensions.cs
@@ -1,3 +1,4 @@
+using System.Runtime.ExceptionServices;
 using Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +10,16 @@ public static class MigrationExtensions
 {
     public static void ApplyMigrations(this IApplicationBuilder app)
     {
-        using var scope = app.ApplicationServices.CreateScope();
-        using var context = scope.ServiceProvider.GetRequiredService<DataContext>();
-        context.Database.Migrate();
+        try
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+            context.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+            var exceptionDispatchInfo = ExceptionDispatchInfo.Capture(ex);
+            exceptionDispatchInfo.Throw();
+        }
     }
 }
