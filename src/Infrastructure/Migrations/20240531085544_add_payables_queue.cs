f64e2c287e3ef963051852e5336ec12f06a72856
fsmaiorano <fsmaiorano@gmail.com>
2024-05-31 16:57:56 +0100

    add migration to create payables queue

---
+++ src/Infrastructure/Migrations/20240531085544_add_payables_queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_payables_queue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PayablesQueue",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BatchId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PayablesBatchQuantity = table.Column<int>(type: "integer", nullable: false),
+                    PayablesProcessed = table.Column<int>(type: "integer", nullable: false),
+                    PayablesProcessedSuccess = table.Column<int>(type: "integer", nullable: false),
+                    PayablesProcessedError = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PayablesQueue", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PayablesQueue");
+        }
+    }
+}
